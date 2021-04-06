80da16966b366425ada670bcb86227f446e6823c
bobowu0716 <>
2021-04-06 10:30:38 +0000

    New code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+/* CSCI1520
+*
+* Assignment:5
+* Student ID:1155142172
+* Name:Wu Xi Bao
+* Email:1155142172@link.cuhk.edu.hk
+*/
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "Hello World!\n";
 }
